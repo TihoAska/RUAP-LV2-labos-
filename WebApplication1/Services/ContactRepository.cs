99d127ac6cc751b1948daa41cdeba228d19743d2
Stjepan Stojcevic <stjepanstojcevic@gmail.com>
2022-11-12 09:55:24 +0100

    Add project files.

---
+++ WebApplication1/Services/ContactRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Services
+{
+    public class ContactRepository
+    {
+        public Contact[] GetAllContacts()
+        {
+            return new Contact[]
+            {
+            new Contact
+            {
+                Id = 1,
+                Name = "Glenn Block"
+            },
+            new Contact
+            {
+                Id = 2,
+                Name = "Dan Roth"
+            }
+            };
+        }
+    }
+}
